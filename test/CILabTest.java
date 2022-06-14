99dfac2a23b9879ac64bde74434c15e016cd8997
Wesley So <wesley.so@bellevuecollege.edu>
2022-06-13 17:05:25 -0700

    Added first test to check for all capitilization by using THIS to see if test would respond true

--- test/CILabTest.java
+++ test/CILabTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class CILabTest {
@@ -20,7 +21,9 @@ public class CILabTest {
 
     @Test
     public void detectCapitalUseTest1() {
-        fail("Not yet implemented");
+        myString.setString("THIS");
+        boolean check = myString.detectCapitalUse();
+        assertEquals(true, check);
 
     }
     @Test
